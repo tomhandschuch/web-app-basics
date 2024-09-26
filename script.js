a7a6e0938c2b5bf425cd90a5550e2dbbe0c534dc
Tom Handschuch <tom.handschuch@gmail.com>
2024-09-26 15:45:17 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const button = document.getElementById("toggleButton");
+const body = document.body;
+
+button.addEventListener("click", function() {
+    body.classList.toggle('dark-mode');
+
+    if (body.classList.contains('dark-mode')) {
+        document.title = "Good Night";
+    } else {
+        document.title = "Good Morning";
+    }
+});
